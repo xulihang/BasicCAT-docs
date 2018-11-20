2add8d5133a1ccc598c7ee263253d4675db30c33
xulihang <xulihanghai@163.com>
2018-11-20 21:42:02 +0800

    add translation

--- source/conf.py
+++ source/conf.py
@@ -35,6 +35,9 @@ release = ''
 #
 # needs_sphinx = '1.0'
 
+locale_dirs = ['locale/']
+gettext_compact = False
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
